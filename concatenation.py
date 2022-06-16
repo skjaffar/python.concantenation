c780a1bdb9e27faca3b56ded5d57e6f47c1f4ed8
skjaffar <107207838+skjaffar@users.noreply.github.com>
2022-06-16 11:13:20 +0530

    Add files via upload

---
+++ concatenation.py
@@ -0,0 +1,26 @@
+# print(''.isspace())
+# print(','.isspace())
+# print('.'.isspace())
+# print('hai'.isspace())
+# print(' '.isspace())
+# print('  '.isspace())
+# print('hai'+''+'hello')
+# print('hai'+' '+'hello')
+# print('wedne','sday')
+# print('wedne',' sday')
+
+
+# print(type(''))
+# print(type(''''''))
+
+# x='hai' ; y='hello'
+# print(x+y)
+# print(y+x)
+
+# print(''+'hai')
+# print(' '+'hello')
+
+# print('hai my name jaffar','😋😊😊')
+
+# print("""    """+'hai')
+# print('hai'+'_'+'hello')   
